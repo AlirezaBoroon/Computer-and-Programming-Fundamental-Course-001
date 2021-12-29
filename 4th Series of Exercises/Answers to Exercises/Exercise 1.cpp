c4d940aeec4f2d4e38dcea772703084bc479fa20
Alireza <77126065+AlirezaBoroon@users.noreply.github.com>
2021-12-29 14:54:00 +0330

    Update Exercise 1.cpp

--- 4th Series of Exercises/Answers to Exercises/Exercise 1.cpp
+++ 4th Series of Exercises/Answers to Exercises/Exercise 1.cpp
@@ -4,8 +4,9 @@ using namespace std;
 // Here we declare and define sum_digits function.
 int sum_digits(int num){
     int sum_of_digits= 0;// This variable stores the summation.
+    int digit;
     while(num>= 1){
-        int digit= num% 10;
+        digit= num% 10;
         // digit variable is actually the digit of the number.
         sum_of_digits= sum_of_digits+ digit;
         num= num/ 10;// Number is devided by 10 for reaching to the digits.
