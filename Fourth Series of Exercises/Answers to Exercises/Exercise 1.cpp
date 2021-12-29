c38b4e6ab411cf04552c036f67c181a46ec2a3fa
Alireza <77126065+AlirezaBoroon@users.noreply.github.com>
2021-12-29 14:23:51 +0330

    Create Exercise 1.cpp

---
+++ Fourth Series of Exercises/Answers to Exercises/Exercise 1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+// Here we declare and define sum of digits function.
+int sum_digits(int num){
+    int sum_of_digits= 0;// This variable stores the sum.
+    while(num>= 1){
+        int digit= num% 10;
+        // digit variable is actually the digit of the number.
+        sum_of_digits= sum_of_digits+ digit;
+        num= num/ 10;// Number is devided by 10 for reaching to the digits.
+    }
+    return sum_of_digits;
+}
+
+int main()
+{
+    cout<< "This program calculates sum of digits of a number given by user."<< endl;
+    cout<< "Please enter the number: ";
+    int number;
+    cin>> number;
+    int sum;
+    sum= sum_digits(number);
+    cout<< "Sum of digits of the number '"<< number<< "' is: "<< sum;
+    return 0;
+}
