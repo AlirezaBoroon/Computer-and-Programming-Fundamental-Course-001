0e76956b88846615dfec77114497d2bd1f5ec70c
Alireza <77126065+AlirezaBoroon@users.noreply.github.com>
2021-12-29 14:29:11 +0330

    Create Exercise 3.cpp

---
+++ 4th Series of Exercises/Answers to Exercises/Exercise 3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+// Here we declare and define positive_num_entry function.
+void Hello_World(int num){
+    // This function returns nothing, so we declare it as a void function.
+    for(int i= 1; i<= num; i++){
+        cout<< "Hello World"<< endl;
+    }
+}
+int main()
+{
+    cout<< "This program prints 'Hello World' for times of a number given by user."<< endl;
+    int number;
+    cout<< "Please enter a number: ";
+    cin>> number;
+    Hello_World(number);
+    cout<< "Process is done.";
+    return 0;
+}
