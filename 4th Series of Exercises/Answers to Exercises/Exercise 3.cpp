b214887db35f8de8685a8615e8a9c683f5bb56ae
Alireza <77126065+AlirezaBoroon@users.noreply.github.com>
2022-01-10 18:41:16 +0330

    Update Exercise 3.cpp

--- 4th Series of Exercises/Answers to Exercises/Exercise 3.cpp
+++ 4th Series of Exercises/Answers to Exercises/Exercise 3.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-// Here we declare and define positive_num_entry function.
+// Here we declare and define Hello_World function.
 void Hello_World(int num){
     // This function returns nothing, so we declare it as a void function.
     for(int i= 1; i<= num; i++){
