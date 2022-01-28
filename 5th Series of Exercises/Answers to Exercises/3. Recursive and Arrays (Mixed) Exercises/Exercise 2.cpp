6093a969fdaebdce5d2da40497731269c241a92f
Alireza <77126065+AlirezaBoroon@users.noreply.github.com>
2022-01-28 14:56:45 +0330

    Create Exercise 2.cpp

---
+++ 5th Series of Exercises/Answers to Exercises/3. Recursive and Arrays (Mixed) Exercises/Exercise 2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int s;
+// "s" variable is declared globally to use it everywhere.
+char * UpperLetter(char arr[], char arr2[], int i=0){
+    if (i == s)
+         return arr2;
+    else
+        arr2[s-1- i]= arr[i];
+    return UpperLetter(arr, arr2, i+1);
+}
+int main(){
+    cout<< "Please enter the size of the array of letters: ";
+    cin>> s;
+    char n[s];
+    char m[s];
+    cout<< "Enter the word with "<< s<< " characters: ";
+    for (int i= 0; i< s; i++){
+        cin>> n[i];
+    }
+    char * inversed= UpperLetter(n, m);
+    cout<< "The word's been reversed and is: ";
+    for (int i = 0; i< s; i++){
+        cout << inversed[i];
+    }
+    return 0;
+}
